71e41962ee1c42f1f674318148a16db671cdef7e
yjt <961549745@qq.com>
2019-03-13 15:07:04 +0800

    自娱自乐了查重版本

--- matlab/R01_mk_paper_in_one.m
+++ matlab/R01_mk_paper_in_one.m
@@ -1,3 +1,8 @@
 clc,clear,close all;
 s=read_tex('../paper/','paper.tex','utf8');
+% 下面的替换为了 pandoc 的生成更科学
+s=strrep(s,'.pdf','.png');
+s=strrep(s,'\renewcommand{\arraystretch}','%\renewcommand{\arraystretch}');
+s=strrep(s,'\bibliography{refs}','\input{paper_in_one.bbl.tex}');
 writetext('../paper/paper_in_one.tex',s,'utf8');
+
