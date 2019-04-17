0502e5c3d70d1a56c3329a669b6af18a53a60bf8
yjt <yu_jiang_tao@qq.com>
2019-04-17 10:26:57 +0800

    蓝瘦

--- matlab/R04_cmp_lump.m
+++ matlab/R04_cmp_lump.m
@@ -3,8 +3,9 @@ f=@(m)double(log10(hypergeom([1, -m, 1/2-m], [], 4)/hypergeom([-m, 1/2-m], [], 2
 x=1:20;
 y=arrayfun(f,x);
 figure('Position',[680   791   438   307]);
-plot(x,y);
+plot(x,y,'-k');
 xlabel('order of lump solution','FontName','Times New Roman','FontSize',16);
 ylabel('${\rm log}_{10}( N_{old}(m) / N_{new}(m) )$','Interpreter','latex');
 saveas(1,'lump_cmp.pdf');
 !pdfcrop lump_cmp.pdf lump_cmp.pdf
+copyfile lump_cmp.pdf ../paper/fig/lump_cmp.pdf
